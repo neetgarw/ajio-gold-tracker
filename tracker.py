b23b0975d7e6ef7aa947d492bb541ebb12cf2199
neetgarw <agarwal_puneet@yahoo.in>
2026-06-11 11:23:10 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import requests
 
 url = "https://www.ajio.com/s/ham-gold-coins-and-bars-4775-71731?query=%3Aprce-asc"
+
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
@@ -8,4 +9,4 @@ headers = {
 r = requests.get(url, headers=headers, timeout=30)
 
 print("STATUS:", r.status_code)
-print(r.text[:10000])
+print(r.text[:5000])
